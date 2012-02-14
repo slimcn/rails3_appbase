1f8eaa7d214b1a4b920f6553400aa6740b8bbee3
slimcn <slimcn@gmail.com>
2012-02-14 15:57:02 +0800

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppBase::Application.config.secret_token = '53668fad0a6e25a7802b7da25df383f2dcfd5cd62b98fc395df7f9f7654c1f9b327df5beb5157c0a77f39099ab2a1441e13ef7c50d20848aef0a65e45840c5d9'
